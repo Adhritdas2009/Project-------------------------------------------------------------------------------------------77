893880e6b766010fa619e1dcc9016d4dcd431252
Adhritdas2009 <89207264+Adhritdas2009@users.noreply.github.com>
2021-10-21 15:04:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var a="Adhrit";
+
+console.log(a);
+
+var b = 100;
+
+var c= 100;
+
+var d= c+b;
+
+console.log(d);
+
+var s= Math.sqrt(4);
+
+console.log(s);
+
+var l= Math.round(12.9343456454454989829832)
+
+console.log(l);
+
+var sub=b-c;
+
+console.log(sub);
